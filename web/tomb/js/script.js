cc7772504a04d85e160f01a4fb7d6dc97a538fb3
Tamás Gavrán <b0gavtam@vasvari.org>
2023-10-13 11:03:12 +0200

    tömb kész

---
+++ web/tomb/js/script.js
@@ -0,0 +1,38 @@
+let tomb = [1,2,3,4,5];
+function elso() 
+{
+    let kiiras = document.getElementById("kiiras");
+    kiiras.textContent = tomb[0];
+}
+function masodik()
+{
+    let kiiras = document.getElementById("kiiras");
+    kiiras.textContent = tomb[1];
+}
+function harmadik() 
+{
+    let kiiras = document.getElementById("kiiras"); 
+    kiiras.textContent = tomb[2];
+}
+function hozzaadas() 
+{  
+    var textarea = document.getElementById("szoveg").value;
+    tomb.push(textarea);
+    
+}
+function osszes()
+{  
+    let kiiras = document.getElementById("kiiras");
+    kiiras.textContent = tomb;
+}
+function hossz() 
+{
+    let kiiras = document.getElementById("kiiras");
+    kiiras.textContent = "A tömb " + tomb.length + " elemet tartalmaz.";
+}
+function torol() 
+{
+    let kiiras = document.getElementById("kiiras");
+    var torol = document.getElementById("torolindex").value;
+    kiiras.textContent = "az eltávolított érték: "+ tomb.splice(torol,1);
+}
