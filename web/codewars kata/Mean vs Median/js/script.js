773bc5f8c5f94ceca88203527e1c61bf3e77d031
Tamás Gavrán <b0gavtam@vasvari.org>
2024-02-02 11:16:47 +0100

    mvsm

--- web/codewars kata/Mean vs Median/js/script.js
+++ web/codewars kata/Mean vs Median/js/script.js
@@ -1,10 +1,23 @@
-
 function meanVsMedian(numbers) {
         let sum = 0;
         for (let i = 0; i < numbers.length; i++) {
             sum += numbers[i];
         }
-        let mean = sum / numbers.length;
+        let mean = Math.floor(sum / numbers.length);
         numbers.sort();
-        let median = numbers[numbers.length/2];
+        let median = Math.floor(numbers.length/2);
+        console.log(mean);
+        console.log(median);
+
+        if (mean > median) {
+            return "mean";
+        }
+        if (median > mean) {
+            return "median";
+        }
+        else{
+            return "same";
+        }
 }
+let n = [1,2,1,3,4,7,7];
+console.log(meanVsMedian(n));
