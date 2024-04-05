974a73d62fa119ef88bf440c3e9ece1f4934e8aa
Tamás Gavrán <b0gavtam@vasvari.org>
2024-04-05 09:40:14 +0200

    fetch methods

--- web/API/script.js
+++ web/API/script.js
@@ -1,19 +1,23 @@
-let div = document.getElementById("div");
-let tid = document.getElementById("txt").value;
-function f(tid) {
-    fetch(`https://hur.webmania.cc/zips/${tid}.json`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json(); //ha az első thennél nincs return a második nem fut le
-      })
-      .then(telepules => {
-        console.log(telepules);
-        div.innerHTML = telepules.zip + "<br>" + telepules.name + "<br>";
-        
-      })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-      });
+function f() {
+  let tid = document.getElementById("txt").value;
+  fetch(`https://hur.webmania.cc/zips/${tid}.json`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); //ha az első thennél nincs return a második nem fut le
+  })
+  .then(zips => {
+    console.log(zips);
+    Irj(zips)
+    
+    
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+}
+function Irj(zips) {
+  let div = document.getElementById("div");
+  div.innerHTML = zips.zip + "<br>" + zips.name + "<br>";
 }
