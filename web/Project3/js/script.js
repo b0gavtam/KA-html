ba21bb08bda3bc7dadaebb0f18b845018f2c8039
Tamás Gavrán <b0gavtam@vasvari.org>
2024-04-12 09:40:32 +0200

    ye

---
+++ web/Project3/js/script.js
@@ -0,0 +1,9 @@
+fetch( 'https://vvri.pythonanywhere.com/api/courses')
+        .then(response => response.json())
+        .then(data => {
+            if (data)
+                data.forEach(element => {
+                    console.log(element)
+            })
+        })
+        .catch(error => console.log("Hiba történt: " + error))
