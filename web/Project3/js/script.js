8d5644dcfd6cd9dda7f5cc83b10cc2c70bc334e7
Tamás Gavrán <b0gavtam@vasvari.org>
2024-05-16 15:15:17 +0200

    async wip

--- web/Project3/js/script.js
+++ web/Project3/js/script.js
@@ -13,145 +13,164 @@ let diaktorlesgomb = document.getElementById("diaktorles");
 let span = 1;
 let tanhead;
 //--------------------------------------------------------------------------------------------------------------------------------------------------
-function Get() {
-    fetch(url, {
-        method: "GET",
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-            "Access-Control-Allow-Origin": "*"
-        }
-    })
-    .then(response => response.json())
-    .then(json => {
-        let li = `<tr><th>ID</th><th>Kurzusnév</th><th id = "tanhead">Tanulók</th></tr>`;
-        json.forEach(course => {
-            if (course.students.length > span) {
-                span = course.students.length;
-            }   
-            li += `<tr>
-            <td id = "kontent">${course.id}</td>
-            <td id = "kontent">${course.name}</td>`;
-            for (let i = 0; i < course.students.length; i++) {
-                li+=`<td id = "kontent">${course.students[i].id},${course.students[i].name}</td>`
-            }
-            li+=`</tr>`
-            
-        });
-        document.getElementById("li").innerHTML = li;
-        tanhead = document.getElementById("tanhead");
-        tanhead.setAttribute("colspan", span);
-    });
+async function Get() {
+    try {
+        await fetch(url, {
+             method: "GET",
+             headers: {
+                 "Content-type": "application/json; charset=UTF-8",
+                 "Access-Control-Allow-Origin": "*"
+             }
+         })
+         const data = await response.json()
+         if (data) {
+             
+         }
+             let li = `<tr><th>ID</th><th>Kurzusnév</th><th id = "tanhead">Tanulók</th></tr>`;
+             data.forEach(course => {
+                 if (course.students.length > span) {
+                     span = course.students.length;
+                 }   
+                 li += `<tr>
+                 <td id = "kontent">${course.id}</td>
+                 <td id = "kontent">${course.name}</td>`;
+                 for (let i = 0; i < course.students.length; i++) {
+                     li+=`<td id = "kontent">${course.students[i].id},${course.students[i].name}</td>`
+                 }
+                 li+=`</tr>`
+                 
+             });
+             document.getElementById("li").innerHTML = li;
+             tanhead = document.getElementById("tanhead");
+             tanhead.setAttribute("colspan", span);
+    } catch (error) {
+        console.log("Hiba történt: " + error);
+    }
 }
 //--------------------------------------------------------------------------------------------------------------------------------------------------
-kurzuskeresgomb.addEventListener("click", function Search() {
-    id= document.getElementById("courseid").value;
-    fetch("https://vvri.pythonanywhere.com/api/courses/" + id)
-        .then(response => response.json())
-        .then(data => {
-            if (data) {
-                document.getElementById("k").innerHTML = "A kurzus neve: " + data.name;
-            } 
-    })
-.catch(error => console.log("Hiba történt: " + error))
-Get();
+kurzuskeresgomb.addEventListener("click", async function Search() {
+    id = document.getElementById("courseid").value;
+    try{
+        await fetch("https://vvri.pythonanywhere.com/api/courses/" + id)
+            const data = await response.json();
+                if (data) {
+                    document.getElementById("k").innerHTML = "A kurzus neve: " + data.name;
+                }
+    }
+    catch(error){
+    console.log("Hiba történt: " + error);
+    }
+    Get();
 })
 kurzuskeresgomb.addEventListener("click", function(){
     document.getElementById("k").style.visibility = "visible";
 })
 //--------------------------------------------------------------------------------------------------------------------------------------------------
-ujkurzusgomb.addEventListener("click", function newCourse() {
+ujkurzusgomb.addEventListener("click", async function newCourse() {
     coursename = document.getElementById("coursename").value;
-    fetch(url, {
-        method: "POST",
-        body: JSON.stringify({
-            name: coursename
-            
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
+    try {
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                name: coursename
+                
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        const data = response.json()
+        data => {
+            console.log("New course created:", data);
+            document.getElementById("nk").innerHTML = "Új kurzus létrehozva."
         }
-    })
-    .then(data => {
-        console.log("New course created:", data);
-        document.getElementById("nk").innerHTML = "Új kurzus létrehozva."
-    })
-    Get();
+        Get();
+        
+    } catch (error) {
+        console.log("Hiba történt: " + error);
+    }
 
 })
 ujkurzusgomb.addEventListener("click", function(){
     document.getElementById("nk").style.visibility = "visible";
 })
 //--------------------------------------------------------------------------------------------------------------------------------------------------
-ujdiakgomb.addEventListener("click", function studentInCourse(studentname, courseid) {
+ujdiakgomb.addEventListener("click", async function studentInCourse(studentname, courseid) {
     studentname = document.getElementById("studentname").value;
     courseid = document.getElementById("course2id").value;
-    fetch(sturl, {
-        method: "POST",
-        body: JSON.stringify({
-            name: studentname,
-            course_id: courseid
-            
-        }),
-         
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data) {
-            console.log("New student in course created:", data);
-            document.getElementById("ndk").innerHTML = "Diák hozzáadva  a kurzushoz: " + data.id + `\t` + data.name;
-        }
-    })
-    .catch(error => console.error("Error creating new course:", error));
-    Get();
+    try {
+        fetch(sturl, {
+            method: "POST",
+            body: JSON.stringify({
+                name: studentname,
+                course_id: courseid
+                
+            }),
+             
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        const data = await response.json()
+            if (data) {
+                console.log("New student in course created:", data);
+                document.getElementById("ndk").innerHTML = "Diák hozzáadva  a kurzushoz: " + data.id + `\t` + data.name;
+            }
+        Get();
+        
+    } catch (error) {
+        console.log("Hiba történt: " + error)
+    }
 })
 ujdiakgomb.addEventListener("click", function(){
     document.getElementById("ndk").style.visibility = "visible";
 })
 //--------------------------------------------------------------------------------------------------------------------------------------------------
-diakkeresgomb.addEventListener("click", function studentToSearch(id) {
+diakkeresgomb.addEventListener("click", async function studentToSearch(id) {
     id= document.getElementById("studentid").value;
-    fetch("https://vvri.pythonanywhere.com/api/students/" + id)
-        .then(response => response.json())
-        .then(data => {
-            if (data) {
-                console.log(data);
-               document.getElementById("diakadat").innerHTML = "Diák neve: " +  data.name;
-            }
-    })
-Get();
+    try {
+        await fetch("https://vvri.pythonanywhere.com/api/students/" + id)
+            const data = await response.json()
+                if (data) {
+                    console.log(data);
+                   document.getElementById("diakadat").innerHTML = "Diák neve: " +  data.name;
+                }
+        
+        Get();
+        
+    } catch (error) {
+        console.log("Hiba történt: " + error);
+    }
 })
 diakkeresgomb.addEventListener("click", function(){
     document.getElementById("diakadat").style.visibility = "visible";
 })
 //--------------------------------------------------------------------------------------------------------------------------------------------------
-diakmodgomb.addEventListener("click", function studentChange(id, studentname, courseid) {
+diakmodgomb.addEventListener("click", async function studentChange(id, studentname, courseid) {
     id = document.getElementById("schange").value;
     studentname = document.getElementById("studentnamemod").value;
     courseid = document.getElementById("changeid").value;
-    fetch("https://vvri.pythonanywhere.com/api/students/" + id, {
-        method: "PUT",
-        body: JSON.stringify({
-            name: studentname,
-            course_id: courseid
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log("Student updated:", data);
-        document.getElementById("diakvaltoztatas").innerHTML = "Diák megváltoztatva." + "<br>" + "Név: " + data.name + "<br>" + "Kurzus: " + data.id;
-    })
-    Get();
+    try {
+        await fetch("https://vvri.pythonanywhere.com/api/students/" + id, {
+           method: "PUT",
+           body: JSON.stringify({
+               name: studentname,
+               course_id: courseid
+           }),
+           headers: {
+               "Content-type": "application/json; charset=UTF-8"
+           }
+       })
+       const data = await response.json();
+       if (data) {
+           console.log("Student updated:", data);
+           document.getElementById("diakvaltoztatas").innerHTML = "Diák megváltoztatva." + "<br>" + "Név: " + data.name + "<br>" + "Kurzus: " + data.id;
+       }
+       Get();
+   }    
+    catch (error) {
+        console.log("Hiba történt: " + error);
+    }
 })
 diakmodgomb.addEventListener("click", function(){
     document.getElementById("diakvaltoztatas").style.visibility = "visible";
