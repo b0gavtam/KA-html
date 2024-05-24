444496c4ec6390e58951c9b2f036f8ba5cf0d26b
Tamás Gavrán <b0gavtam@vasvari.org>
2024-05-24 09:41:21 +0200

    kész node

--- web/node2/szoveg.js
+++ web/node2/szoveg.js
@@ -1,21 +1,29 @@
 const fs = require('fs')
-fs.writeFile('./web/node2/szoveg.txt', 
+if (!fs.existsSync('./web/node2/assets')) {
+    fs.mkdir('./web/node2/assets', err => {
+      if (err) {
+        console.log(err);
+      }
+      console.log('mappa létrehozva.');
+    });
+} 
+fs.writeFile('./web/node2/assets/szoveg.txt', 
 `All this running around 
-I cant fight it much longer
-Somethings tryin to get out
-And its never been closer
+I can't fight it much longer
+Somethings tryin' to get out
+And it's never been closer
 
 If my take-off fails
 Make up some other story
 But if I never come back
-Tell my mother Im sorry
+Tell my mother I'm sorry
 
-Baby now Im ready moving on
+Baby, now Im ready moving on
 Oh, but maybe I was ready all along
 Oh, Im ready for the moment and the sound
 Oh, but maybe I was ready all along
 
-Baby now Im ready, moving on
+Baby, now I'm ready, moving on
 Oh, but maybe I was ready all along
 Oh, Im ready for the moment and the sound
 Oh, but maybe I was ready all along`, (err) =>
@@ -25,7 +33,7 @@ Oh, but maybe I was ready all along`, (err) =>
         console.log(err)
     }
 })
-fs.readFile('./web/node2/szoveg.txt', (err, data) => {
+fs.readFile('./web/node2/assets/szoveg.txt', (err, data) => {
     if (err) {
       console.log(err);
     }  
