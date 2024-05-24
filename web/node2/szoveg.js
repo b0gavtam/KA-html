bf8c18860f38998ccdb1f1fcd8fe5325a3ab6d42
Tamás Gavrán <b0gavtam@vasvari.org>
2024-05-24 09:28:15 +0200

    dal spacing fix

--- web/node2/szoveg.js
+++ web/node2/szoveg.js
@@ -4,14 +4,17 @@ fs.writeFile('./web/node2/szoveg.txt',
 I cant fight it much longer
 Somethings tryin to get out
 And its never been closer
+
 If my take-off fails
 Make up some other story
 But if I never come back
 Tell my mother Im sorry
+
 Baby now Im ready moving on
 Oh, but maybe I was ready all along
 Oh, Im ready for the moment and the sound
 Oh, but maybe I was ready all along
+
 Baby now Im ready, moving on
 Oh, but maybe I was ready all along
 Oh, Im ready for the moment and the sound
