aa066a176335c2f5b68aed6af9933ac20f5c6357
Tamás Gavrán <b0gavtam@vasvari.org>
2024-09-19 12:30:08 +0200

    problem

--- web/Codewars API/js/script.js
+++ web/Codewars API/js/script.js
@@ -22,7 +22,7 @@ overallKeresGomb.addEventListener("click", async function overallSearch() {
                 if (data) {
                     let li;
                     li=  `Összesített adatok: <br>`;
-                    li += `Rank: ${data.ranks.overall.rank} <br>`
+                    li += `Rank: ${data.ranks.overall.rank * -1} <br>`
                     li += `Név: ${data.ranks.overall.name}  <br>`
                     li += `Szín: ${data.ranks.overall.color}  <br>`
                     li += `Pontok: ${data.ranks.overall.score}  <br>`
@@ -50,22 +50,26 @@ languageKeresGomb.addEventListener("click", async function languageSearch() {
             }
         })
 
-            const data = await response.json();
-                
-                if (data) {
-                    let li = "";
-                    for (let i = 0; i < data.ranks.languages.length; i++) {
-                        li += `Rank: ${data.ranks.languages[i].rank} + <br>`
-                        li += `Név: ${data.ranks.languages[i].name} + <br>`
-                        li += `Szín: ${data.ranks.languages[i].color} + <br>`
-                        li += `Pontok: ${data.ranks.languages[i].score} + <br>` 
-                    }
-                    document.getElementById("nk").innerHTML = li;
-                }
-                
+            const data = await response.json();  
+            let li;
+            let obj = new Object(data.ranks.languages)
+            li += obj;
+            
+            console.log(data.ranks.languages);
+            /*for (let i = 0; i < data.ranks.languages; i++) {
+                li += `Rank: ${languages[i].rank} + <br>`
+                li += `Név: ${languages[i].name} + <br>`
+                li += `Szín: ${languages[i].color} + <br>`
+                li += `Pontok: ${languages[i].score} + <br>`
+             
+            }*/
+           
+                        
+                document.getElementById("nk").innerHTML = li;     
     }
     catch(error){
-        console.log(error)
+        error = ("Error 404 - User not found")
+        document.getElementById("nk") = error;
     }
     setTimeout(200)
 });
